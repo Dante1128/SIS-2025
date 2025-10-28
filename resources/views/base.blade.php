40edfb4b3aa73ff4a3cf4f1519eef4f830decda6
Dante1128 <danteyupanqui2811@gmail.com>
2025-10-28 17:45:05 -0400

    doc/update-readme

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sidebar Simple</title>
+    <title>Inicio</title>
 </head>
 <body>
 
