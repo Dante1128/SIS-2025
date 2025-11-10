c0e6ff3717cc39a6ffd1d509a5708e6014d32703
ThaisFlores111 <floresgutierrezthaisdaniela@gmail.com>
2025-11-09 23:15:45 -0400

    cambio

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -1,24 +1,19 @@
 <!DOCTYPE html>
 <html lang="es" style="width: 100%; height: 100%;">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
 </head>
-
 <body>
-
     <!-- Sidebar -->
     <div style="width: 250px; display: flex; flex-direction: column;border-right: 10px solid #CCE54F;background-color: #152039;color:#ffffff">
         <!-- Imagen superior -->
         <div style="margin-bottom: 20px;">
             <img src="{{ asset('img/logo-emi.png') }}" alt="Logo" style="width: 85%; padding:30px 20px 20px 20px;">
         </div>
-
-        <!-- Links -->
-    
+        <!-- Links -->   
         <a href="#" style="margin-bottom: 10px;text-decoration: none;padding: 17px 5px 10px 21px;color:#ffffff;font-size:18px;"><i class="fa-solid fa-building" style="padding-right:10px;"></i>Departamentos</a>
         <a href="#" style="margin-bottom: 10px;text-decoration: none;padding: 17px 5px 10px 21px;color:#ffffff;font-size:18px;"><i class="fa-solid fa-book-open" style="padding-right:10px;"></i>Programas</a>
         <a href="#" style="margin-bottom: 10px;text-decoration: none;padding: 17px 5px 10px 21px;color:#ffffff;font-size:18px;"><i class="fa-solid fa-graduation-cap" style="padding-right:10px;"></i>Curso</a>
@@ -26,16 +21,12 @@
         <a href="#" style="margin-bottom: 10px;text-decoration: none;padding: 17px 5px 10px 21px;color:#ffffff;font-size:18px;"><i class="fa-solid fa-layer-group" style="padding-right:10px;"></i>Area</a>
         <a href="{{ route('dominioSubdominio') }}" style="margin-bottom: 10px;padding: 17px 5px 10px 21px;;text-decoration: none;color:#ffffff;font-size:18px;"><i class="fa-solid fa-diagram-project" style="padding-right:10px;"></i>Dominio y Subdomino</a>
     </div>
-
     <!-- Contenido principal -->
     <div style="flex-grow: 1; padding: 20px;">
         @yield('content')
     </div>
-
     <main class="content">
         @yield('content')
     </main>
-
 </body>
-
 </html>
